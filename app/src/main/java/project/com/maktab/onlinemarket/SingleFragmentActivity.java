d916820af3be20db68239e636c99d748f068fd10
Sepehr Sadri <sepehrsadri@gmail.com>
2019-02-11 13:51:29 +0330

    + initial commit + single fragment activity created.

---
+++ app/src/main/java/project/com/maktab/onlinemarket/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package project.com.maktab.onlinemarket;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    public abstract Fragment createFragment();
+
+    @LayoutRes
+    public abstract int getLayoutResId();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, createFragment())
+                .commit();
+
+
+    }
+}
