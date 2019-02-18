b69f185523a72fd22368923e0a326c512138f839
Sepehr Sadri <sepehrsadri@gmail.com>
2019-02-18 21:07:53 +0330

    + Search Api method and search view item added to main market fragment.

--- app/src/main/java/project/com/maktab/onlinemarket/network/Api.java
+++ app/src/main/java/project/com/maktab/onlinemarket/network/Api.java
@@ -37,5 +37,7 @@ public interface Api {
     @GET("products/?consumer_key=ck_00fdf4e3f65c5275d802b412db586ba2cac6835f&consumer_secret=cs_d2571d995db502ea4b04bfae270b92ac447eb8ba")
     Call<List<Product>> getProductsSubCategoires(@Query("category") String categoryId);
 
+    @GET("products/?consumer_key=ck_00fdf4e3f65c5275d802b412db586ba2cac6835f&consumer_secret=cs_d2571d995db502ea4b04bfae270b92ac447eb8ba")
+    Call<List<Product>> searchProducts(@Query("search") String productName);
 
 }
