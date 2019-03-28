c23dc197b2f8d38dd92dd9f08ca38180aa0ccdd8
Sepehr Sadri <sepehrsadri@gmail.com>
2019-03-28 18:03:22 +0430

    + choose time menu added to main market menu and time choose dialog fragment and layout first initial is completed.

---
+++ app/src/main/java/project/com/maktab/onlinemarket/controller/fragment/TimeChooseDialogFragment.java
@@ -0,0 +1,85 @@
+package project.com.maktab.onlinemarket.controller.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import project.com.maktab.onlinemarket.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeChooseDialogFragment extends DialogFragment {
+    @BindView(R.id.submit_time_btn)
+    Button mSubmitBtn;
+    @BindView(R.id.custom_time_edit_text)
+    EditText mCustomTimeEditText;
+    @BindView(R.id.custom_time_input_layout)
+    TextInputLayout mCustomTimeInputLayout;
+    @BindView(R.id.selected_time_spinner)
+    Spinner mSelectTimeSpinner;
+
+
+    public static TimeChooseDialogFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        TimeChooseDialogFragment fragment = new TimeChooseDialogFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    public TimeChooseDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_time_choose_dialog, container, false);
+        ButterKnife.bind(this,view);
+
+
+
+
+
+        return view;
+    }
+
+    private boolean validateTimeEditText() {
+        if (mCustomTimeEditText.getText().toString().trim().isEmpty()) {
+            mCustomTimeInputLayout.setError(getString(R.string.empty_error));
+            requestFocus(mCustomTimeEditText);
+            return false;
+        } else {
+            mCustomTimeInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+
+
+}
